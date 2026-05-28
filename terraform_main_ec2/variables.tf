849a50351f9a0cbc957edb4e15bf1ee9874334fe
Rameshwaram Varun Teja <tejavarun689@gmail.com>
2026-05-28 14:14:25 +0530

    Changed ec2 key

--- terraform_main_ec2/variables.tf
+++ terraform_main_ec2/variables.tf
@@ -76,7 +76,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "EC2 keypair"
   type        = string
-  default     = "us-east-1"   // Replace with the keypair
+  default     = "threetier"   // Replace with the keypair
 }
 
 variable "instance_name" {
